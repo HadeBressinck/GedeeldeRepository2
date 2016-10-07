514d659e872cb712f64b578049d5adb8a264f9f6
Hade Bressinck <hade.bressinck@hotmail.com>
2016-10-07 15:23:05 +0200

    java file voor Jeroen met Person class

---
+++ PersonClassForGRepo2.java
@@ -0,0 +1,9 @@
+public class Person {
+
+	private int firstName;
+	private String LastName;
+	
+	public static void main(String[] args){
+		System.out.println("This is "+firstName+" "+LastName");
+	}
+}
